d231da51ca4041235b7833cbf8ff3f3084a895a4
Felicitas Pojtinger <felicitas@pojtinger.com>
2026-04-04 00:39:36 -0700

    feat: Add noscript support for expandable sections
    Signed-off-by: Felicitas Pojtinger <felicitas@pojtinger.com>

--- assets/js/expandable-section.js
+++ assets/js/expandable-section.js
@@ -1,7 +1,20 @@
-for (const section of document.querySelectorAll(".pf-v6-c-expandable-section")) {
-  const toggle = section.querySelector(".pf-v6-c-expandable-section__toggle button");
+for (const section of document.querySelectorAll(
+  ".pf-v6-c-expandable-section",
+)) {
+  const toggle = section.querySelector(
+    ".pf-v6-c-expandable-section__toggle button",
+  );
   const content = section.querySelector(".pf-v6-c-expandable-section__content");
   const textEl = toggle.querySelector(".pf-v6-c-button__text");
+
+  // Invert the default visibility if JS is available and add corresponding ARIA tags
+  // If no JS is available, everything is visible and the ARIA attributes aren't necessary
+  toggle.setAttribute("aria-expanded", "false");
+  toggle.setAttribute("aria-controls", toggle.dataset.controls);
+  content.setAttribute("role", "region");
+  content.setAttribute("aria-labelledby", toggle.id);
+
+  content.hidden = true;
 
   toggle.addEventListener("click", () => {
     const expanded = toggle.getAttribute("aria-expanded") === "true";
